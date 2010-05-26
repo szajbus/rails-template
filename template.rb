76710215566b2b2948c2b13608711e131616a1c4
Michał Szajbe <michal.szajbe@gmail.com>
2010-05-26 02:38:56 +0200

    added preinitializer to setup bundler

--- template.rb
+++ template.rb
@@ -20,7 +20,31 @@ file "Gemfile", <<-END
 source :gemcutter
 gem 'rails', '~> 2.3.8'
 END
+
 run "bundle install vendor/bundle"
+
+file "config/preinitializer.rb", <<-END
+begin
+  require "rubygems"
+  require "bundler"
+rescue LoadError
+  raise "Could not load the bundler gem. Install it with `gem install bundler`."
+end
+
+if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9.24")
+  raise RuntimeError, "Your bundler version is too old." +
+    "Run `gem install bundler` to upgrade."
+end
+
+begin
+  # Set up load paths for all bundled gems
+  ENV["BUNDLE_GEMFILE"] = File.expand_path("../../Gemfile", __FILE__)
+  Bundler.setup
+rescue Bundler::GemNotFound
+  raise RuntimeError, "Bundler couldn't find some gems." +
+    "Did you run `bundle install`?"
+end
+END
 
 # Set up git repository
 git :init
